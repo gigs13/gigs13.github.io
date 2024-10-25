a626cc5c3f2d4cf1f9f567eb14d82dea8a16728a
Rodrigo Sánchez Isunza <rodrigoisunza@gmail.com>
2024-10-25 11:20:41 -0600

    Add features to projectCard

--- app/src/components/proyecto.js
+++ app/src/components/proyecto.js
@@ -3,8 +3,9 @@ import {
   CardHeader,
   CardContent,
   CardActions,
-  Typography,
   Button,
+  Typography,
+  Tooltip,
 } from "@mui/material";
 import { motion } from "framer-motion";
 
@@ -16,20 +17,61 @@ export default function Proyecto({ project }) {
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: -50 }}
       transition={{ duration: 0.3 }}
+      whileHover={{ scale: 1.05 }} // Efecto hover
+      whileTap={{ scale: 1.1 }} // Efecto tap para agrandar temporalmente
     >
-      <Card sx={{ margin: "20px" }}>
+      <Card
+        elevation={3}
+        sx={{
+          maxWidth: 500,
+          margin: "auto",
+          transition: "0.3s",
+        }}
+      >
         <CardHeader
           title={
             <Typography variant="h4" color={"text.secondary"}>
               {project.title}
             </Typography>
           }
-        ></CardHeader>
+          subheader={
+            // Tooltip para cada tecnología
+            project["tech-stack"].map((tech) => (
+              <Tooltip title={tech.category} key={tech.id}>
+                <Typography
+                  variant="body2"
+                  color="text.secondary"
+                  component="span"
+                  sx={{ mr: 1 }} // Espaciado entre cada tecnología
+                >
+                  {tech.language}
+                </Typography>
+              </Tooltip>
+            ))
+          }
+        />
+
         <CardContent>
-          <Typography variant="body2">{project.description}</Typography>
+          <Typography variant="body2" color="text.secondary">
+            {project.description}
+          </Typography>
         </CardContent>
+
         <CardActions>
-          <Button size="small">Learn More</Button>
+          <Button
+            variant="contained"
+            size="small"
+            onClick={() => console.log("GitHub clicked")}
+          >
+            GitHub
+          </Button>
+          <Button
+            variant="contained"
+            size="small"
+            onClick={() => console.log("Demo clicked")}
+          >
+            Demo
+          </Button>
         </CardActions>
       </Card>
     </motion.div>
