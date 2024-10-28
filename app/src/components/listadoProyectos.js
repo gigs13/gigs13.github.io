936cc4c1c78e1cc17c9617d110ba47b9db7293d9
Rodrigo Sánchez Isunza <rodrigoisunza@gmail.com>
2024-10-28 14:04:57 -0600

    Fix filter show or not project

--- app/src/components/listadoProyectos.js
+++ app/src/components/listadoProyectos.js
@@ -5,10 +5,12 @@ import { useContext } from "react";
 import { motion } from "framer-motion";
 
 export default function Proyectos() {
-  const projects = useContext(UserContext).projects;
+  const projects = useContext(UserContext).projects.filter(
+    (project) => project.show
+  );
 
   return (
-    <Grid container spacing={2}>
+    <Grid container spacing={2} justifyContent="space-between">
       {projects.map((project) => (
         <Grid item xs={12} sm={6} md={4} key={project.id}>
           <motion.div
